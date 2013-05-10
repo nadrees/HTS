1743c9f663513b3d666c71671ffbc4f1f2fbdf95
Nathen Drees <nadrees@gmail.com>
2013-05-10 17:43:56 -0500

    Completed Programming Challenge 1

---
+++ Prog 1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Prog_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<WordDetails> words = BuildWords("wordlist.txt");
+            IEnumerable<WordDetails> scrambledWords = BuildWords("scrambledWords.txt");
+
+            var unscrambledWords = UnscrambleWords(scrambledWords, words);
+            File.WriteAllLines("result.txt", new[] { String.Join(",", unscrambledWords.ToArray()) });
+        }
+
+        private static IEnumerable<String> UnscrambleWords(IEnumerable<WordDetails> scrambledWords, IEnumerable<WordDetails> words)
+        {
+            List<String> unscrambledWords = new List<String>();
+
+            foreach (var scrambledWord in scrambledWords)
+            {
+                String word = words.First(w => scrambledWord.EqualsCharCount(w)).Word;
+                unscrambledWords.Add(word);
+            }
+
+            return unscrambledWords;
+        }
+
+        private static IEnumerable<WordDetails> BuildWords(String fileName)
+        {
+            List<WordDetails> words = new List<WordDetails>();
+
+            foreach (String line in File.ReadAllLines(fileName))
+            {
+                var chars = line.ToCharArray();
+                WordDetails details = new WordDetails(line);
+                foreach (var currentChar in chars)
+                    details.IncrementCount(currentChar);
+
+                words.Add(details);
+            }
+
+            return words;
+        }
+    }
+}
---
+++ Prog 1/WordDetails.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_1
+{
+    public class WordDetails
+    {
+        public String Word { get; private set; }
+        private Dictionary<char, int> charCounts;
+
+        public WordDetails(String word)
+        {
+            this.Word = word;
+            charCounts = new Dictionary<char, int>();
+        }
+
+        public void IncrementCount(char c)
+        {
+            if (!charCounts.ContainsKey(c))
+                charCounts.Add(c, 0);
+            charCounts[c] += 1; 
+        }
+
+        public int GetCount(char c)
+        {
+            return charCounts.ContainsKey(c) ? charCounts[c] : 0;
+        }
+
+        public override string ToString()
+        {
+            String charCountsDesc = String.Join(", ", charCounts.Keys.Select(k => String.Format("{0}:{1}", k, charCounts[k])));
+
+            return String.Format("{0}:[{1}]", Word, charCountsDesc);
+        }
+
+        public bool EqualsCharCount(WordDetails other)
+        {
+            foreach (var charCountKey in charCounts.Keys)
+            {
+                if (other.GetCount(charCountKey) != GetCount(charCountKey))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
